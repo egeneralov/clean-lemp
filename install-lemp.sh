5486a097f1c1c37876858dce8d60728837fd8735
root <root@nginx.ga>
2017-04-03 00:42:50 +0300

    ssl fix

--- install-lemp.sh
+++ install-lemp.sh
@@ -25,3 +25,6 @@ debconf-set-selections <<< "mysql-server-5.5	mysql-server/root_password_again st
 
 echo "Installing LEMP (may cost time)";
 apt-get install -y nginx mysql-server php7.0 php7.0-cli php7.0-fpm php7.0-curl php7.0-gd php7.0-imagick php7.0-mcrypt php7.0-imap php7.0-mbstring php7.0-xml php7.0-xmlrpc php7.0-xsl php7.0-json php7.0-memcached php7.0-apcu php7.0-apcu-bc php7.0-opcache php7.0-mysql php7.0-sqlite3 php7.0-pgsql php7.0-redis php7.0-interbase php7.0-odbc php7.0-zip php7.0-bz2 php7.0-dba php7.0-enchant php7.0-gmp php7.0-igbinary php7.0-intl php7.0-ldap php7.0-msgpack php7.0-geoip php7.0-readline php7.0-recode php7.0-tidy > /dev/null 2>&1;
+
+#ssl
+mkdir /etc/nginx/ssl
